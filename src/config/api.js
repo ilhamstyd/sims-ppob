d7175448994196daae80dc8149c488a74565c920
unknown <ilhamsetyadji28@gmail.com>
2023-08-25 07:05:29 +0700

    done

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: "https://take-home-test-api.nutech-integrasi.app",
 })
 
 export const setAuthToken = (token) => {
